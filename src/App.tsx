703149c4de8850553149428599f70513a4abe73e
Pedro Brasca <pedropaulobrasca@gmail.com>
2025-04-01 22:19:11 -0300

    Instalado e configurado o ShadcnUI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='text-2xl'>Vite + React</h1>
+      <Button>Teste</Button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
